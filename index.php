955bedb458d1835292599d9638119807cee96254
Cécile Dubois <duboiscecilepro@gmail.com>
2022-05-23 18:42:12 +0200

    Retrieved the data from distant website

--- index.php
+++ index.php
@@ -6,4 +6,9 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 $folder = $_ENV['FOLDER'];
 
-echo $folder;
+// data request
+$data = file_get_contents('https://www.maisonapart.com/article.json');
+$dataDecoded = json_decode($data);
+$docs = $dataDecoded->response->docs;
+
+var_dump($docs);
